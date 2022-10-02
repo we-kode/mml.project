a86adfcaf7b00f2c8c16a75a2f188b00dfa8ca88
stummk <stummk@users.noreply.github.com>
2022-10-02 21:29:06 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://we-kode.github.io',
-  baseUrl: '/',
+  baseUrl: '/mml.project/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
