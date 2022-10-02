b3509a63f6ef4162c30ec6b88d8ad0c61db2e5fb
stummk <stummk@users.noreply.github.com>
2022-10-02 21:26:36 +0200

    removed blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -104,10 +104,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
               },
