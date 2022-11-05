a026ba9e5559b5ff9d65d3ac5462dd971831affe
stummk <stummk@users.noreply.github.com>
2022-11-05 15:28:49 +0100

    Updated theme and added sections

--- docs/intro.md
+++ docs/intro.md
@@ -1,8 +1,9 @@
 ---
+slug: /
 sidebar_position: 1
 ---
 
-# Tutorial Intro
+# Project description
 
 Let's discover **Docusaurus in less than 5 minutes**.
 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'My Media Lib',
+  tagline: 'Self-Hosted, Private, Everywhere Available media library',
   url: 'https://we-kode.github.io',
   baseUrl: '/mml.project/',
   onBrokenLinks: 'throw',
@@ -34,11 +34,8 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
         theme: {
@@ -52,20 +49,25 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
         items: [
           {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentation',
           },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            label: 'Board',
+            href: 'https://github.com/orgs/we-kode/projects/1',
+            position: 'right',
+          },
+          {
+            label: 'Issues',
+            href: 'https://github.com/we-kode/mml.project/issues',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/we-kode?q=mml.&type=all&language=&sort=',
             label: 'GitHub',
             position: 'right',
           },
@@ -73,44 +75,8 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} WeKoDe`,
+        links: [],
       },
       prism: {
         theme: lightCodeTheme,
